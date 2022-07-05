f36a56fd41969093e9135a231fda070883b557cc
kairzhan <kkural@dar.io>
2022-07-05 15:34:02 +0600

    spm

--- SPM/ViewController.swift
+++ SPM/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+        let title = UILabel()
+        title.textColor = .yellow
+        title.text = "Hello, World!"
+        title.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(title)
+        NSLayoutConstraint.activate([
+            title.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            title.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        let image = UIImageView()
+        image.image = .init(systemName: "star.fill")
+        view.addSubview(image)
+        image.snp.makeConstraints {
+            $0.top.equalTo(title.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
     }
 
 
